628e1ce0951bf2cca78c2b8ed2a3f18810c0aa11
unknown <mikefreesycamore@comcast.net>
2020-06-14 16:39:09 -0400

    use quotes

--- scp-tomcat-prod.bat
+++ scp-tomcat-prod.bat
@@ -1,3 +1,3 @@
 where scp
-C:\WINDOWS\System32\OpenSSH\scp.exe -i C:\Users\mike\Downloads\tomcat-demo.pem **/target/*.war ec2-user@3.19.221.237:/var/lib/tomcat8/webapps
+C:\WINDOWS\System32\OpenSSH\scp.exe -i C:\Users\mike\Downloads\tomcat-demo.pem "C:\Program Files (x86)\Jenkins\workspace\FullyAutomated\webapp\target\*.war" ec2-user@3.19.221.237:/var/lib/tomcat8/webapps
 
