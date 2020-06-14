7fd3214d07129b1134e33a467539252b471cbcbf
unknown <mikefreesycamore@comcast.net>
2020-06-14 16:45:09 -0400

    add slash

--- scp-tomcat-stag.bat
+++ scp-tomcat-stag.bat
@@ -1,2 +1,2 @@
 where scp
-C:\WINDOWS\System32\OpenSSH\scp.exe -i "C:\Users\mike\Downloads\tomcat-demo.pem C:\Program Files (x86)\Jenkins\workspace\FullyAutomated\webapp\target\*.war" ec2-user@3.15.232.162:/var/lib/tomcat8/webapps
+C:\WINDOWS\System32\OpenSSH\scp.exe -i "C:\Users\mike\Downloads\tomcat-demo.pem C:\Program Files (x86)\Jenkins\workspace\FullyAutomated\webapp\target\*.war" ec2-user@3.15.232.162:/var/lib/tomcat8/webapps/
