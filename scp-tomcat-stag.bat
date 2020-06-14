f8792c255708d64933842bea548fc199d1370766
unknown <mikefreesycamore@comcast.net>
2020-06-14 12:55:12 -0400

    add drive to path

--- scp-tomcat-stag.bat
+++ scp-tomcat-stag.bat
@@ -1,2 +1,2 @@
 where scp
-C:/WINDOWS/System32/OpenSSH/scp -i /Users/mike/Downloads/tomcat-demo.pem **/target/*.war ec2-user@3.15.232.162:/var/lib/tomcat8/webapps
+C:/WINDOWS/System32/OpenSSH/scp -i C:/Users/mike/Downloads/tomcat-demo.pem **/target/*.war ec2-user@3.15.232.162:/var/lib/tomcat8/webapps
